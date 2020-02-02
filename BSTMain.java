fa07b5e7d8834926765d8d2df9056a7a9bcf4c02
darecc <46760315+darecc@users.noreply.github.com>
2020-02-02 13:44:14 +0100

    Add files via upload

---
+++ BSTMain.java
@@ -0,0 +1,15 @@
+package dc.main;
+
+import struktury.BST;
+import struktury.BSTNode;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class BSTMain {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+       
+    }
+}
